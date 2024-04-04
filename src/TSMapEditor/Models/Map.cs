7a6c5cb1a542b35450c0ab66c2f0c82f55e081b8
Starkku <starkku.ppmod@gmail.com>
2024-04-04 16:51:10 +0300

    Pass theater dropdown item as index instead of name to allow using any name in dropdown

--- src/TSMapEditor/Models/Map.cs
+++ src/TSMapEditor/Models/Map.cs
@@ -209,7 +209,7 @@ namespace TSMapEditor.Models
             EditorConfig.EarlyInit();
         }
 
-        public void InitNew(GameConfigINIFiles gameConfigINIFiles, string theaterName, Point2D size, byte startingLevel)
+        public void InitNew(GameConfigINIFiles gameConfigINIFiles, int theaterIndex, Point2D size, byte startingLevel)
         {
             const int marginY = 6;
             const int marginX = 4;
@@ -218,6 +218,7 @@ namespace TSMapEditor.Models
             InitializeRules(gameConfigINIFiles);
             LoadedINI = new IniFileEx();
             var baseMap = new IniFileEx(Environment.CurrentDirectory + "/Config/BaseMap.ini", ccFileManager);
+            var theaterName = EditorConfig.Theaters.Count > theaterIndex ? EditorConfig.Theaters[theaterIndex].UIName : string.Empty;
             baseMap.FileName = string.Empty;
             baseMap.SetStringValue("Map", "Theater", theaterName);
             baseMap.SetStringValue("Map", "Size", $"0,0,{size.X},{size.Y}");

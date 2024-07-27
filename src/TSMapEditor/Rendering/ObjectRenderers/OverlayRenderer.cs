3e74286a67b4de536a989c4caa8c2fecb41fd19d
Starkku <starkku.ppmod@gmail.com>
2024-07-27 21:11:05 +0300

    Don't render replacement text for low bridge graphics

--- src/TSMapEditor/Rendering/ObjectRenderers/OverlayRenderer.cs
+++ src/TSMapEditor/Rendering/ObjectRenderers/OverlayRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using SharpDX.Mathematics.Interop;
 using TSMapEditor.GameMath;
 using TSMapEditor.Models;
 
@@ -59,6 +60,22 @@ namespace TSMapEditor.Rendering.ObjectRenderers
             DrawShadow(gameObject);
             DrawShapeImage(gameObject, drawParams.ShapeImage, gameObject.FrameIndex, Color.White,
                 true, remapColor, affectedByLighting, affectedByAmbient, drawPoint);
+        }
+
+        protected override bool ShouldRenderReplacementText(Overlay gameObject)
+        {
+            OverlayType overlay = gameObject.OverlayType;
+
+            foreach (var bridge in Map.EditorConfig.Bridges)
+            {
+                if (bridge.Kind == BridgeKind.Low &&
+                    (bridge.EastWest.Pieces.Contains(overlay) || bridge.NorthSouth.Pieces.Contains(overlay)
+                    || bridge.EastWest.Start == overlay || bridge.EastWest.End == overlay
+                    || bridge.NorthSouth.Start == overlay || bridge.NorthSouth.End == overlay))
+                    return false;
+            }
+
+            return base.ShouldRenderReplacementText(gameObject);
         }
     }
 }

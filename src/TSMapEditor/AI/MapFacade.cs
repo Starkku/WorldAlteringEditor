0f8bde7a6368f07fbf05d3112362ef971fba0049
Rampastring <rampastring@outlook.com>
2026-08-01 14:06:59 +0300

    Add techno information to MCP cell information

--- src/TSMapEditor/AI/MapFacade.cs
+++ src/TSMapEditor/AI/MapFacade.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using TSMapEditor.GameMath;
 using TSMapEditor.Models;
 using TSMapEditor.Mutations;
@@ -33,9 +34,61 @@ public class MapOverlayInfo : MapObjectInfo
     public int FrameID { get; }
 }
 
+public class MapTechnoInfo : MapObjectInfo
+{
+    public MapTechnoInfo(string rtti, int x, int y, string iniName, string owner, byte facing, int hp, string attachedTag) : base(rtti, x, y, iniName)
+    {
+        Owner = owner;
+        Facing = facing;
+        HP = hp;
+        AttachedTag = attachedTag;
+    }
+
+    public int HP { get; }
+    public string AttachedTag { get; }
+    public string Owner { get; }
+    public byte Facing { get; }
+}
+
+public class MapBuildingInfo : MapTechnoInfo
+{
+    public MapBuildingInfo(string rtti, int x, int y, string iniName, string owner, byte facing, int hp, string attachedTag, bool powered, bool aiRepairable)
+        : base(rtti, x, y, iniName, owner, facing, hp, attachedTag)
+    {
+        Powered = powered;
+        AIRepairable = aiRepairable;
+    }
+
+    public bool Powered { get; }
+    public bool AIRepairable { get; }
+}
+
+public class MapFootInfo : MapTechnoInfo
+{
+    public MapFootInfo(string rtti, int x, int y, string iniName, string owner, byte facing, int hp, string attachedTag, string mission,
+        bool onBridge, int veterancy, int group, bool autocreateNoRecruitable, bool autocreateYesRecruitable)
+        : base(rtti, x, y, iniName, owner, facing, hp, attachedTag)
+    {
+        Mission = mission;
+        OnBridge = onBridge;
+        Veterancy = veterancy;
+        Group = group;
+        AutocreateNoRecruitable = autocreateNoRecruitable;
+        AutocreateYesRecruitable = autocreateYesRecruitable;
+    }
+
+    public string Mission { get; }
+    public bool OnBridge { get; }
+    public int Veterancy { get; }
+    public int Group { get; }
+    public bool AutocreateNoRecruitable { get; }
+    public bool AutocreateYesRecruitable { get; }
+}
+
 public class CellInfo
 {
-    public CellInfo(int x, int y, string tileSetName, int tileIndexInTileSet, int height, MapObjectInfo terrainObjectInfo, MapOverlayInfo overlayInfo)
+    public CellInfo(int x, int y, string tileSetName, int tileIndexInTileSet, int height, MapObjectInfo terrainObjectInfo, MapOverlayInfo overlayInfo,
+        List<MapBuildingInfo> buildingInfos, List<MapFootInfo> footInfos)
     {
         X = x;
         Y = y;
@@ -44,6 +97,8 @@ public class CellInfo
         Height = height;
         TerrainObjectInfo = terrainObjectInfo;
         OverlayInfo = overlayInfo;
+        BuildingInfos = buildingInfos;
+        FootInfos = footInfos;
     }
 
     public int X { get; }
@@ -53,6 +108,8 @@ public class CellInfo
     public int Height { get; }
     public MapObjectInfo TerrainObjectInfo { get; }
     public MapOverlayInfo OverlayInfo { get; }
+    public List<MapBuildingInfo> BuildingInfos { get; }
+    public List<MapFootInfo> FootInfos { get; }
 
     public static CellInfo FromMapCell(ITheater theater, MapTile mapTile)
     {
@@ -61,8 +118,12 @@ public class CellInfo
 
         var terrainObjectInfo = mapTile.TerrainObject == null ? null : new MapObjectInfo(RTTIType.Terrain.ToString(), mapTile.TerrainObject.Position.X, mapTile.TerrainObject.Position.Y, mapTile.TerrainObject.TerrainType.ININame);
         var overlayInfo = mapTile.Overlay == null ? null : new MapOverlayInfo(mapTile.Overlay.Position.X, mapTile.Overlay.Position.Y, mapTile.Overlay.OverlayType.ININame, mapTile.Overlay.FrameIndex);
+        var buildingInfos = mapTile.Structures.Select(s => new MapBuildingInfo(s.WhatAmI().ToString(), s.Position.X, s.Position.Y, s.ObjectType.ININame, s.Owner.ININame, s.Facing, s.HP, s.AttachedTag?.Name, s.Powered, s.AIRepairable)).ToList();
+        var vehicleInfos = mapTile.Vehicles.Select(v => new MapFootInfo(v.WhatAmI().ToString(), v.Position.X, v.Position.Y, v.ObjectType.ININame, v.Owner.ININame, v.Facing, v.HP, v.AttachedTag?.Name, v.Mission, v.High, v.Veterancy, v.Group, v.AutocreateNoRecruitable, v.AutocreateYesRecruitable));
+        var infantryInfos = mapTile.Infantry.Where(i => i != null).Select(i => new MapFootInfo(i.WhatAmI().ToString(), i.Position.X, i.Position.Y, i.ObjectType.ININame, i.Owner.ININame, i.Facing, i.HP, i.AttachedTag?.Name, i.Mission, i.High, i.Veterancy, i.Group, i.AutocreateNoRecruitable, i.AutocreateYesRecruitable));
+        var aircraftInfos = mapTile.Aircraft.Select(a => new MapFootInfo(a.WhatAmI().ToString(), a.Position.X, a.Position.Y, a.ObjectType.ININame, a.Owner.ININame, a.Facing, a.HP, a.AttachedTag?.Name, a.Mission, a.High, a.Veterancy, a.Group, a.AutocreateNoRecruitable, a.AutocreateYesRecruitable));
 
-        return new CellInfo(mapTile.X, mapTile.Y, tileSet.SetName, mapTile.TileIndex - tileSet.StartTileIndex, mapTile.Level, terrainObjectInfo, overlayInfo);
+        return new CellInfo(mapTile.X, mapTile.Y, tileSet.SetName, mapTile.TileIndex - tileSet.StartTileIndex, mapTile.Level, terrainObjectInfo, overlayInfo, buildingInfos, vehicleInfos.Concat(infantryInfos).Concat(aircraftInfos).ToList());
     }
 }
 

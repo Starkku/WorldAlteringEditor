138b5ff4284ea76ed3337a6c1b7b3e99fabb38fa
Rampastring <rampastring@outlook.com>
2026-08-02 23:02:18 +0300

    Add ending pieces to DTA connected tile config

--- src/TSMapEditor/Constants.cs
+++ src/TSMapEditor/Constants.cs
@@ -4,7 +4,7 @@ namespace TSMapEditor
 {
     public static class Constants
     {
-        public const string ReleaseVersion = "1.9.0";
+        public const string ReleaseVersion = "1.8.999";
 
         public static int CellSizeX = 48;
         public static int CellSizeY = 24;

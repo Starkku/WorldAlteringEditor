aa33b39c6469c8d5fc599dce36b76c02b11cdfe4
Rampastring <rampastring@outlook.com>
2026-08-09 12:41:24 +0300

    Don't copy local runtime target assets to build output directory

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@
 
 $Output = Join-Path $PSScriptRoot Build
 
-dotnet publish src/TSMapEditor/TSMapEditor.csproj --configuration=Release --no-self-contained --output=$Output
+dotnet publish src/TSMapEditor/TSMapEditor.csproj --configuration=Release --no-self-contained -p:CopyLocalRuntimeTargetAssets=false --output=$Output
